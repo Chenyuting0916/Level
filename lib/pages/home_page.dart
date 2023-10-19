784509811e8a725aec866143dc7baf86432a4be3
昱廷 陳 <chenyuting0916@hotmail.com>
2023-10-19 20:23:22 +0800

    add scaffold for every page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:level/pages/profile_page.dart';
+import 'package:level/pages/ranking_page.dart';
+import 'package:level/pages/settings_page.dart';
+import 'package:level/pages/timer_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List _pages = [
+    const ProfilePage(),
+    const TimerPage(),
+    const RankingPage(),
+    const SettingsPage()
+  ];
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: Container(
         color: Colors.black,
-        child: const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
           child: GNav(
-            backgroundColor: Colors.black,
-            color: Colors.white,
-            activeColor: Colors.white,
+              selectedIndex: _selectedIndex,
+              onTabChange: _navigateBottomBar,
+              backgroundColor: Colors.black,
+              color: Colors.white,
+              activeColor: Colors.white,
               gap: 4,
-              padding: EdgeInsets.all(16),
-              tabs: [
+              padding: const EdgeInsets.all(16),
+              tabs: const [
                 GButton(
                   icon: Icons.person,
                   text: 'Profile',
@@ -37,6 +63,7 @@ class HomePage extends StatelessWidget {
               ]),
         ),
       ),
+      body: _pages[_selectedIndex],
     );
   }
 }
---
+++ lib/pages/timer_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TimerPage extends StatelessWidget {
+  const TimerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('timer'),
+    );
+  }
+}
