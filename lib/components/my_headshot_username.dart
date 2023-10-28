586b52149023764749f6ce5690a35bf2ea335c2a
昱廷 陳 <chenyuting0916@hotmail.com>
2023-10-28 11:28:43 +0800

    update username

--- lib/components/my_headshot_username.dart
+++ lib/components/my_headshot_username.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:level/components/my_button.dart';
+import 'package:level/services/user_service.dart';
 
-class MyHeadshotAndUsername extends StatelessWidget {
-  final String username;
+// ignore: must_be_immutable
+class MyHeadshotAndUsername extends StatefulWidget {
+  String username;
   final String imageUrl;
-  const MyHeadshotAndUsername(
+  MyHeadshotAndUsername(
       {super.key, required this.username, required this.imageUrl});
 
   @override
+  State<MyHeadshotAndUsername> createState() => _MyHeadshotAndUsernameState();
+}
+
+class _MyHeadshotAndUsernameState extends State<MyHeadshotAndUsername> {
+  var toggleEdit = false;
+
+  @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController(text: widget.username);
     return Padding(
       padding: const EdgeInsets.only(top: 15, bottom: 12),
       child: Row(
@@ -19,16 +30,73 @@ class MyHeadshotAndUsername extends StatelessWidget {
             child: CircleAvatar(
               backgroundColor: Theme.of(context).colorScheme.secondary,
               radius: 37,
-              backgroundImage: AssetImage(imageUrl),
+              backgroundImage: AssetImage(widget.imageUrl),
             ),
           ),
           const SizedBox(
             width: 18,
           ),
-          Text(
-            username,
-            style: const TextStyle(fontSize: 24),
-          ),
+          Visibility(
+              visible: !toggleEdit,
+              child: Row(
+                children: [
+                  Tooltip(
+                    message: widget.username,
+                    triggerMode: TooltipTriggerMode.tap,
+                    child: Text(
+                      widget.username.length > 10
+                          ? '${widget.username.substring(0, 10)}...'
+                          : widget.username,
+                      style: const TextStyle(fontSize: 24),
+                    ),
+                  ),
+                  MyButton(
+                    onPressed: () {
+                      setState(() {
+                        toggleEdit = !toggleEdit;
+                      });
+                    },
+                    buttonChild: const Icon(Icons.edit),
+                    buttonBackgroundColor: Colors.transparent,
+                  )
+                ],
+              )),
+          Visibility(
+              visible: toggleEdit,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    child: TextField(
+                      controller: textController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  MyButton(
+                    onPressed: () {
+                      UserService()
+                          .updateUser({"username": textController.value.text});
+                      setState(() {
+                        toggleEdit = !toggleEdit;
+                        widget.username = textController.value.text;
+                      });
+                    },
+                    buttonChild: const Icon(Icons.save),
+                    buttonBackgroundColor: Colors.transparent,
+                  ),
+                  MyButton(
+                    onPressed: () {
+                      setState(() {
+                        toggleEdit = !toggleEdit;
+                      });
+                    },
+                    buttonChild: const Icon(Icons.keyboard_return_outlined),
+                    buttonBackgroundColor: Colors.transparent,
+                  )
+                ],
+              ))
         ],
       ),
     );
