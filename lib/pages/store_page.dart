c718af746cfdbaa42eaf98ceaaae66707247bf57
昱廷 陳 <chenyuting0916@hotmail.com>
2023-11-22 01:41:45 +0800

    check in temp

--- lib/pages/store_page.dart
+++ lib/pages/store_page.dart
@@ -3,7 +3,11 @@ import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:level/components/my_appbar.dart';
 import 'package:localization/localization.dart';
 
-const List<String> _projectIds = <String>["premium_level"];
+const List<String> _projectIds = <String>[
+  'premium_level',
+  "premium_level2",
+  "premium monthly"
+];
 
 class StorePage extends StatefulWidget {
   const StorePage({super.key});
@@ -16,8 +20,13 @@ class _StorePageState extends State<StorePage> {
   final InAppPurchase _inAppPurchase = InAppPurchase.instance;
   bool _isAvailable = false;
   String? _notice;
+  List<ProductDetails> _products = [];
 
-  // initState
+  @override
+  void initState() {
+    super.initState();
+    initStoreInfo();
+  }
 
   Future<void> initStoreInfo() async {
     final bool isAvailable = await _inAppPurchase.isAvailable();
@@ -29,6 +38,26 @@ class _StorePageState extends State<StorePage> {
         _notice = "There is no upgrades at this time";
       });
       return;
+    }
+
+    setState(() {
+      _notice = "There is a connection to the store";
+    });
+
+    ProductDetailsResponse productDetailsResponse =
+        await _inAppPurchase.queryProductDetails(_projectIds.toSet());
+    setState(() {
+      _products = productDetailsResponse.productDetails;
+    });
+
+    if (productDetailsResponse.error != null) {
+      setState(() {
+        _notice = "There was a problem connecting to store.";
+      });
+    } else if (productDetailsResponse.productDetails.isEmpty) {
+      setState(() {
+        _notice = "There is no products at this time";
+      });
     }
   }
 
@@ -47,9 +76,37 @@ class _StorePageState extends State<StorePage> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(_notice!),
-            )
+            ),
+          Expanded(
+              child: ListView.builder(
+            itemBuilder: (context, index) {
+              final ProductDetails productDetails = _products[index];
+
+              return Card(
+                child: Column(
+                  children: [
+                    Text(
+                      productDetails.title,
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                    Text(productDetails.description)
+                  ],
+                ),
+              );
+            },
+            itemCount: _products.length,
+          )),
         ],
       )),
     );
+  }
+
+  Widget _getIAPIcon(productId) {
+    if (productId == "") {
+      return Icon(Icons.brightness_7_outlined);
+    }
+    else{
+      return Icon(Icons.brightness_7_outlined);
+    }
   }
 }
